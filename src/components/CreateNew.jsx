96a65bdc97723de506ffd51a3beabb13d34269b0
AguirreEze <aguirre.eze95@gmail.com>
2022-01-02 11:04:07 -0300

    feat 7.4: anecdotes and hooks step1

--- src/components/CreateNew.jsx
+++ src/components/CreateNew.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { useField } from '../hooks'
 
 const CreateNew = ({ addNew, notification }) => {
   const history = useHistory()
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-    notification(`A new anecdote ${content} created!`)
+    notification(`A new anecdote ${content.value} created!`)
     history.push('/')
   }
 
@@ -25,15 +27,15 @@ const CreateNew = ({ addNew, notification }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' value={content.value} onChange={content.onChange} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' value={author.value} onChange={author.onChange} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input name='info' value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
       </form>
